1c33a0d4751d03060df03383d87300d672d345c1
volodymyr.liashuk <volodymyr.liashuk@himumsaiddad.com>
2021-01-06 12:34:43 +0200

    added .env file to store keys

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default class WeatherService {
     static async getWeatherData(latitude, longtitude) {
-        return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=c59e1bc11b91625ebad3a3b30c889468&units=metric`);
+        return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${process.env.REACT_APP_GOOGLE_APIKEY}&units=metric`);
     }
 }
